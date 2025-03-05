7cedbf7a1ac9c8676d451afec1038e5d8f022dc7
DHATRI C <dhatri.c22@gmail.com>
2025-03-05 16:48:32 +0530

    final

--- src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
+++ src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -20,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/applicationLog")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class ApplicationController {
     @Autowired
     private  ApplicationService applicationService;
--- src/main/java/com/cars24/taskmanagement/backend/data/response/TaskDetails.java
+++ src/main/java/com/cars24/taskmanagement/backend/data/response/TaskDetails.java
@@ -26,13 +26,7 @@ public class TaskDetails {
 
     private String id;
 
-    public String getParentId() {
-        return parentId;
-    }
 
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
 
     public String getTaskId() {
         return taskId;
@@ -42,37 +36,8 @@ public class TaskDetails {
         this.taskId = taskId;
     }
 
-    public String getVersion() {
-        return version;
-    }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
 
-    public int getOrder() {
-        return order;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    public String getTemplateId() {
-        return templateId;
-    }
-
-    public void setTemplateId(String templateId) {
-        this.templateId = templateId;
-    }
-
-    public String getTemplateVersion() {
-        return templateVersion;
-    }
-
-    public void setTemplateVersion(String templateVersion) {
-        this.templateVersion = templateVersion;
-    }
 
     public String getFunnel() {
         return funnel;
@@ -82,29 +47,13 @@ public class TaskDetails {
         this.funnel = funnel;
     }
 
-    public String getChannel() {
-        return channel;
-    }
 
-    public void setChannel(String channel) {
-        this.channel = channel;
-    }
 
-    public String getProductType() {
-        return productType;
-    }
 
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
 
-    public String getApplicationId() {
-        return applicationId;
-    }
 
-    public void setApplicationId(String applicationId) {
-        this.applicationId = applicationId;
-    }
+
+
 
     public String getEntityIdentifier() {
         return entityIdentifier;
@@ -112,22 +61,6 @@ public class TaskDetails {
 
     public void setEntityIdentifier(String entityIdentifier) {
         this.entityIdentifier = entityIdentifier;
-    }
-
-    public String getEntityType() {
-        return entityType;
-    }
-
-    public void setEntityType(String entityType) {
-        this.entityType = entityType;
-    }
-
-    public String getActorType() {
-        return actorType;
-    }
-
-    public void setActorType(String actorType) {
-        this.actorType = actorType;
     }
 
     public String getActorId() {
@@ -146,37 +79,9 @@ public class TaskDetails {
         this.status = status;
     }
 
-    public String getExecutionType() {
-        return executionType;
-    }
 
-    public void setExecutionType(String executionType) {
-        this.executionType = executionType;
-    }
 
-    public Map<String, Object> getMetadata() {
-        return metadata;
-    }
 
-    public void setMetadata(Map<String, Object> metadata) {
-        this.metadata = metadata;
-    }
-
-    public Map<String, Object> getInputResourceValueMap() {
-        return inputResourceValueMap;
-    }
-
-    public void setInputResourceValueMap(Map<String, Object> inputResourceValueMap) {
-        this.inputResourceValueMap = inputResourceValueMap;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
 
     public Date getUpdatedAt() {
         return updatedAt;
@@ -186,33 +91,16 @@ public class TaskDetails {
         this.updatedAt = updatedAt;
     }
 
-    public String getHandledBy() {
-        return handledBy;
-    }
 
-    public void setHandledBy(String handledBy) {
-        this.handledBy = handledBy;
-    }
-
-    private String parentId;
     private String taskId;
-    private String version;
-    private int order;
-    private String templateId;
-    private String templateVersion;
+
     private String funnel;
-    private String channel;
-    private String productType;
-    private String applicationId;
+
     private String entityIdentifier;
-    private String entityType;
-    private String actorType;
+
     private String actorId;
     private String status;
-    private String executionType;
-    private Map<String, Object> metadata;
-    private Map<String, Object> inputResourceValueMap;
-    private Date createdAt;
+
     private Date updatedAt;
-    private String handledBy;
+
 }
