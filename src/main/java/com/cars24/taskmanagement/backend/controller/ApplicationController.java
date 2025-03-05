f8c43d06ddd31a2bd357d1b8abe5b9de842c73b0
Aditya Sharma <adityasharma121415@gmail.com>
2025-03-05 22:37:39 +0530

    updated cors

--- src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
+++ src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
@@ -3,10 +3,7 @@ package com.cars24.taskmanagement.backend.controller;
 
 import com.cars24.taskmanagement.backend.service.impl.ApplicationServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/applicationLog")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173/")
 public class ApplicationController {
 
 
