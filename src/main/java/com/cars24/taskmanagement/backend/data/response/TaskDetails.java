b1005c1f45d4ae78d2fc017c388827ac9658222f
DHATRI C <dhatri.c22@gmail.com>
2025-03-10 19:03:00 +0530

    dhatrifeatures

--- src/main/java/com/cars24/taskmanagement/backend/data/response/TaskDetails.java
+++ src/main/java/com/cars24/taskmanagement/backend/data/response/TaskDetails.java
@@ -19,6 +19,45 @@ public class TaskDetails {
 
     private String taskId;
 
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getFunnel() {
+        return funnel;
+    }
+
+    public void setFunnel(String funnel) {
+        this.funnel = funnel;
+    }
+
+    public String getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(String actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     private String funnel;
 
