7778aa21124fbfc48340f297767cf61633911984
Keshav S <keshav.srisha02@gmail.com>
2025-03-10 22:38:07 +0530

    Changed the response format

--- src/main/java/com/cars24/taskmanagement/backend/controller/SlaController.java
+++ src/main/java/com/cars24/taskmanagement/backend/controller/SlaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/SLAMonitoring")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:5173")
 public class SlaController {
 
     private final SlaServiceImpl slaService;
