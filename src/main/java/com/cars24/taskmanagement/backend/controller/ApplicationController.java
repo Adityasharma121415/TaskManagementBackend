87185c4a134197f8e96bbd3a0fc80b41bd188ec3
DHATRI C <dhatri.c22@gmail.com>
2025-03-12 12:24:50 +0530

    added duration controller

--- src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
+++ src/main/java/com/cars24/taskmanagement/backend/controller/ApplicationController.java
@@ -1,15 +1,19 @@
 package com.cars24.taskmanagement.backend.controller;
 
 
+import com.cars24.taskmanagement.backend.data.entity.LoanDuration;
 import com.cars24.taskmanagement.backend.data.response.ApiResponse;
 import com.cars24.taskmanagement.backend.data.response.RepeatedApiResponse;
 import com.cars24.taskmanagement.backend.data.response.TasksResponse;
 import com.cars24.taskmanagement.backend.data.response.dto.TaskGroupedResponse;
+import com.cars24.taskmanagement.backend.service.LoanDurationService;
 import com.cars24.taskmanagement.backend.service.impl.ApplicationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/applicationLog")
@@ -48,5 +52,15 @@ public class ApplicationController {
 
         return ResponseEntity.ok(response);
     }
+    @Autowired
+    private LoanDurationService loanDurationService;
+    @GetMapping("/duration/{applicationId}")
+
+
+    public ResponseEntity<?> getLoanDuration(@PathVariable String applicationId) {
+        Optional<LoanDuration> loanDuration = loanDurationService.fetchLoanDuration(applicationId);
+        return loanDuration.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+}
 }
 
