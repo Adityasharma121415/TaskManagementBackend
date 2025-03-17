f40f82570267386a8f5d7098676f3cea40738086
Keshav S <keshav.srisha02@gmail.com>
2025-03-17 17:58:18 +0530

    Made changes to the sla service IMpl

--- src/main/java/com/cars24/taskmanagement/backend/controller/SlaController.java
+++ src/main/java/com/cars24/taskmanagement/backend/controller/SlaController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins="http://localhost:5173")
+
 @RestController
 @RequestMapping("/SLAMonitoring")
 @RequiredArgsConstructor
