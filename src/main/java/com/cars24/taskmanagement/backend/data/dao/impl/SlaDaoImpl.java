f166aea39fa338dbf1f15155fe9d9efb912234e6
rickvibhadhini <rickvi03@gmail.com>
2025-03-17 12:01:59 +0530

    Checked with new application id

--- src/main/java/com/cars24/taskmanagement/backend/data/dao/impl/SlaDaoImpl.java
+++ src/main/java/com/cars24/taskmanagement/backend/data/dao/impl/SlaDaoImpl.java
@@ -5,18 +5,16 @@ import com.cars24.taskmanagement.backend.data.dao.SlaDao;
 import com.cars24.taskmanagement.backend.data.entity.TaskExecutionTimeEntity;
 import com.cars24.taskmanagement.backend.data.repository.TaskExecutionTimeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SlaDaoImpl  implements SlaDao {
 
     private final TaskExecutionTimeRepository repository;
-
-//    public SlaDaoImpl(TaskExecutionTimeRepository repository) {
-//        this.repository = repository;
-//    }
 
     public List<TaskExecutionTimeEntity> getTasksByChannel(String channel) {
         return repository.findByChannel(channel);
