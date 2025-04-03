f157dd378a522d9a6b47ee1cefbd6a91aa1d34bf
50073 <a50073@L-CFSPL-IN-50073.local>
2025-04-03 18:13:10 +0530

    Added authentication

---
+++ src/main/java/com/cars24/taskmanagement/backend/service/UserDetailService.java
@@ -0,0 +1,30 @@
+package com.cars24.taskmanagement.backend.service;
+
+import com.cars24.taskmanagement.backend.data.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getEmail(),
+                        user.getPassword(),
+                        new ArrayList<>() // Empty authorities list (you can modify this if needed)
+                ))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
